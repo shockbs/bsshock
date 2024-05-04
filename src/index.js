784da99220e47575ef85226e6e3998e8a9733c38
shockbs <apipherng@gmail.com>
2024-05-04 16:45:00 +0800

    e

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-const { readdirSync } = require(`fs`);const { join } = require(`path`);for (let t of readdirSync(join(__dirname, `exports`)).filter(f =>!f.includes("."))){readdirSync(join(__dirname, `exports`, type)).filter(f => f.endsWith(".js") && !["index.js","save.js"].includes(f)).map(f => { exports[f.split(".")[0]] = require(`${__dirname}/exports/${t}/${f}`)})}
+const { readdirSync } = require('fs');
+const { join } = require('path');
+
+for (let t of readdirSync(join(__dirname, 'exports')).filter(f => !f.includes('.'))) {
+    readdirSync(join(__dirname, 'exports', t)).filter(f => f.endsWith('.js') && !['index.js', 'save.js'].includes(f)).map(f => {
+        exports[f.split('.')[0]] = require(`${__dirname}/exports/${t}/${f}`);
+    });
+}
