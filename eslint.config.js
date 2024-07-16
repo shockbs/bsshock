69a1b5f39820072838a42a50a5c79c6531eee18d
shockbs <apipherng@gmail.com>
2024-07-16 19:45:32 +0800

    g

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended"
+    ],
+    "overrides": [
+        {
+            "files": ["*.cjs", "*.js"],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-console": "warn",
+        "no-undef": "error",
+        "semi": "off",
+        "semi-spacing": "error",
+        "arrow-spacing": "off",
+        "no-confusing-arrow": "error",
+        "no-duplicate-imports": "error",
+        "no-var": "error",
+        "object-shorthand": "off",
+        "prefer-const": "error",
+        "prefer-template": "warn",
+        "no-unused-vars": "off"
+    }
+}
