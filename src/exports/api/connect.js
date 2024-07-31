348584c14654c4378bbd92998d6f4226df68c217
shockbs <apipherng@gmail.com>
2024-07-31 15:18:41 +0800

    t

--- src/exports/api/connect.js
+++ src/exports/api/connect.js
@@ -1,16 +1,30 @@
-const { connect, connected, getToken } = require("../../utilman.js")
+const { connect, connected, getToken } = require("../../utilman.js");
 
-module.exports = (a)=> {
+let lastConnectTime = 0;
+
+module.exports = (a) => {
+    const currentTime = Date.now();
+    const cooldownPeriod = 2 * 60 * 60 * 1000;
+
     if (connected()) {
-        console.log(`[${new Date().toString()}] Triggered Another Login to api.shockbs.is-a.dev`)
-        console.log('DO NOT CONSTANTLY Run connect(), you should only run it once.')
+        console.log(`[${new Date().toString()}] Triggered Another Login to api.shockbs.is-a.dev`);
+        console.log('DO NOT CONSTANTLY Run connect(), you should only run it once.');
+
         if (a !== null && getToken() === a) {
-            throw new Error("Given the same credentials");
+            throw new Error("Given the same credentials. The token is already connected");
         }
-        setTimeout(()=> {
+        
+        if (currentTime - lastConnectTime < cooldownPeriod) {
+            console.log('DO NOT CONSTANTLY Run connect(), you should only run it once.');
+            return;
+        }
+
+        lastConnectTime = currentTime;
+        setTimeout(() => {
             return connect(a);
         }, 30000);
     } else {
+        lastConnectTime = currentTime;
         return connect(a);
     }
-}
+};
